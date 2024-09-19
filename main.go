18d1655e13011ec3b879fa4187974a58e56e0cbc
hamidwise1 <hrawebdev@gmail.com>
2024-09-19 13:07:11 +0330

    add api tests and event handlers

--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/", func(c *gin.Context) {
+	server.GET("/events", func(c *gin.Context) {
+		events := models.GetAllEvents()
+		c.JSON(http.StatusOK, events)
+	})
+
+	server.POST("/events", func(c *gin.Context) {
+		var event models.Event
+		err := c.ShouldBindJSON(&event)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "fill required fields",
+			})
+			return
+		}
+		event.ID = 1
+		event.UserID = 1
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello world",
+			"message": "event created",
+			"event":   event,
 		})
+		event.Save()
 	})
 
 	server.Run(":8080")
